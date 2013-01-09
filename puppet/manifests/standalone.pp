0ee7076950b6d0a908ee88649df9d833892b7b58
Stephan Hügel <urschrei@gmail.com>
2013-01-09 18:29:05 +0000

    Adding Puppet Labs postgres manifest
    This installs Postgres, and creates a db and credentials for our app
    Port forwarding is set up from guest 5432 to localhost 15432

--- puppet/manifests/standalone.pp
+++ puppet/manifests/standalone.pp
@@ -4,11 +4,12 @@
 
 import 'lib/*.pp'
 
+include stdlib
 include fabric
 include git
 include gunicorn
 include nginx
-include postgresql
+include postgresql::server
 include python
 include sass
 include uglifyjs
@@ -16,4 +17,14 @@ include vagrant
 
 nginx::site { 'gunicorn':
   config => 'gunicorn',
+}
+# this is required to build Psycopg2
+#package { [ 'postgresql-server-dev-all' ]:
+#  ensure => 'installed',
+#  }
+# creates a blank db for our app to use
+postgresql::db{ 'app_db':
+  user          => 'flask_user',
+  password      => 'flask_pass',
+  grant         => 'all',
 }
