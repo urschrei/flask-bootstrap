7e344c7cb02f9fea1bdce770937023b91ae337a6
Stephan Hügel <urschrei@gmail.com>
2013-05-16 21:10:10 +0100

    Removing unused imports and adding spaces

--- apps/example/models.py
+++ apps/example/models.py
@@ -2,14 +2,13 @@ from flask import current_app
 from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import validates
 from sqlalchemy.sql import expression
-from sqlalchemy.ext.compiler import compiles
-from sqlalchemy.types import DateTime
 # the db instance and mixins can be provided to any blueprint
 from apps.shared.models import db, AppMixin
 
 # auto-generated index names use the ix_table_column naming convention
 
 # Example model
+
 
 # class Example(db.Model, AppMixin):
 #     """ An example model """
