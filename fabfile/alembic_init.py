812b41aeb265d2572bae534edb5ca1ac874762fb
Stephan Hügel <urschrei@gmail.com>
2013-05-28 20:17:42 +0100

    Ensure PsycoPG2 is installed by Puppet
    This allows us to perform an initial DB migration run
    if need be

--- fabfile/alembic_init.py
+++ fabfile/alembic_init.py
@@ -3,6 +3,8 @@ from fabric.decorators import task
 from fabric.context_managers import settings, hide
 from fabric.colors import cyan
 from utils import do
+from __init__ import upgrade_db
+
 
 config_file_path = 'db/alembic.ini'
 
@@ -11,6 +13,6 @@ def build():
     """Initialise and migrate database to latest version."""
     print(cyan('\nUpdating database...'))
     with settings(hide('warnings'), warn_only=True):
-        do('venv/bin/alembic -c %s init db/postgresql' % config_file_path)
-        if not os.path.exists("db/postgresql/versions"):
-            os.mkdir("db/postgresql/versions")
+        if not os.path.exists(config_file_path):
+            do('alembic -c %s init db/postgresql' % config_file_path)
+        upgrade_db()
--- puppet/modules/fabric/manifests/init.pp
+++ puppet/modules/fabric/manifests/init.pp
@@ -1,4 +1,12 @@
 class fabric {
+  package { 'pip':
+    provider => 'pip',
+    ensure   => 'present',
+  }
+  package { 'psycopg2':
+    provider => 'pip',
+    ensure   => 'present',
+  }
   package { 'Fabric':
     provider => 'pip',
     ensure   => 'present',
